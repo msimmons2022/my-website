cae730f387aac3c6dd00111cfc02047cf14e2a1f
Draethius <54371536+msimmons2022@users.noreply.github.com>
2023-09-25 19:30:27 -0500

    Started Responsive Design
    Started adding all style sets to the CSS file, removing inline styling to keep all styles in one place.
    
    Started creating Media Queries to get the window size of the device used, and changing website formatting based off of that. Changing style to have mobile first design. INCOMPLETE

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Navigation bar script
-    window.onscroll = function() {navFunction()};
-
-    var navBar = document.getElementById("navBar");
-    var sticky = navBar.offsetTop;
-
-    function navFunction() {
-        if(window.pageYOffset >= sticky){
-            navBar.classList.add("sticky")
-        } else {
-            navBar.classList.remove("sticky");
-        }
-    }
+    
 })
